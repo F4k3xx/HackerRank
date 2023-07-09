6f6154a15f376b203ed763cd3ba25936ea9e575e
Wellynton Spagnol <wellyntonspagnol10@gmail.com>
2023-07-08 21:33:20 -0300

    Different solution

---
+++ src/main/java/org/example/JavaStdinAndStoudI/SolutionSimple.java
@@ -0,0 +1,16 @@
+package org.example.JavaStdinAndStoudI;
+
+import java.util.Scanner;
+
+public class SolutionSimple {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
